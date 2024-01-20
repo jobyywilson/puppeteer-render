de88bcd2946413ffb9b9b193080ec95969e053b1
Joby Wilson Mathews <35163814+jobyywilson@users.noreply.github.com>
2024-01-20 14:24:03 +0530

    Update scrapeLogic.js

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -18,27 +18,11 @@ const scrapeLogic = async (res) => {
 
     await page.goto("https://developer.chrome.com/");
 
-    // Set screen size
-    await page.setViewport({ width: 1080, height: 1024 });
-
-    // Type into search box
-    await page.type(".search-box__input", "automate beyond recorder");
-
-    // Wait and click on first result
-    const searchResultSelector = ".search-box__link";
-    await page.waitForSelector(searchResultSelector);
-    await page.click(searchResultSelector);
-
-    // Locate the full title with a unique string
-    const textSelector = await page.waitForSelector(
-      "text/Customize and automate"
-    );
-    const fullTitle = await textSelector.evaluate((el) => el.textContent);
-
-    // Print the full title
-    const logStatement = `The title of this blog post is ${fullTitle}`;
-    console.log(logStatement);
-    res.send(logStatement);
+    const textContent = await page.evaluate(() => {
+      const h2Element = document.getElementById('a-powerful-web-span-stylecolor-var-chrome-primarymade-easierspan');
+      return h2Element ? h2Element.textContent : null;
+    });
+    res.send(textContent);
   } catch (e) {
     console.error(e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
